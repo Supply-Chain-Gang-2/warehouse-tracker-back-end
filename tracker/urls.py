55db6bb817dc847c0b0297df530b6390516e48f4
SkylerJohnson102020 <basseuph@gmail.com>
2021-07-19 12:05:00 -0700

    Tracker apps.py and urls.py updates. Starting work on templates.

--- tracker/urls.py
+++ tracker/urls.py
@@ -3,7 +3,7 @@ from .views import HomePageView, AboutUsPageView, SearchPage
 
 
 urlpatterns = [
-  path('', HomePageView.as_view(), name='home.html')
+  path('', HomePageView.as_view(), name='home.html'),
+  path('about/', AboutUsPageView.as_view(), name='about.html'),
+  path('search/', SearchPage.as_view(), name='search.html'),
 ]
-
-
