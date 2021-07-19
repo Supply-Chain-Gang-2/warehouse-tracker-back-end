b4a237d9df5e0b4e40a1f7c642be5ec8d4f4a60e
Zach Hornung <Zachhornung@comcast.net>
2021-07-19 11:28:55 -0700

    front end is set up, back end is set up, we are monolithic

--- tracker/urls.py
+++ tracker/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from warehouses.views import WarehouseList, WarehouseDetail
+from .views import HomePageView, AboutUsPageView, SearchPage
+
 
 urlpatterns = [
-  path("", WarehouseList.as_view(), name='warehouse_list'),
-  path("<int:pk>/", WarehouseDetail.as_view(), name='warehouse_detail'),
+  path('', HomePageView.as_view(), name='home.html')
 ]
+
+
