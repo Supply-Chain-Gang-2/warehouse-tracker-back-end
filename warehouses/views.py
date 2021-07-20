9b8d24d7473f313775f531a69b6ae77ba6872c95
Kyle Hoac <hoackyle@gmail.com>
2021-07-20 11:58:39 -0700

    DO NOT PUSH TO DEV OR MAIN

--- warehouses/views.py
+++ warehouses/views.py
@@ -10,9 +10,13 @@ from .serializers import WarehouseSerializer
 class WarehouseList(ListCreateAPIView):
     queryset = Warehouse.objects.all()
     serializer_class = WarehouseSerializer
-
+    template_name = "my_warehouses.html"
+    model = Warehouse
 
 class WarehouseDetail(RetrieveUpdateDestroyAPIView):
     permission_classes = (IsOwnerOrReadOnly,)
     queryset = Warehouse.objects.all()
     serializer_class = WarehouseSerializer
+    template_name = "my_warehouses.html"
+    model = Warehouse
+
