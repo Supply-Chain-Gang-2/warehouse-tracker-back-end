23f924896c801baaa98ff55ae7cefd2015043fb9
Kyle Hoac <hoackyle@gmail.com>
2021-07-19 17:34:57 -0700

    started styling the home page, in the middle of adding background image

--- theme/static_src/tailwind.config.js
+++ theme/static_src/tailwind.config.js
@@ -12,6 +12,7 @@ module.exports = {
      * Set mode to "jit" if you want to generate your styles on-demand as you author your templates;
      * Set mode to "aot" if you want to generate the stylesheet in advance and purge later (aka legacy mode).
      */
+
     mode: "jit",
 
     purge: [
@@ -22,13 +23,13 @@ module.exports = {
         /*  Templates within theme app (<tailwind_app_name>/templates), e.g. base.html. */
         '../templates/**/*.html',
 
-        /* 
+        /*
          * Main templates directory of the project (BASE_DIR/templates).
          * Adjust the following line to match your project structure.
          */
         '../../templates/**/*.html',
-        
-        /* 
+
+        /*
          * Templates in other django apps (BASE_DIR/<any_app_name>/templates).
          * Adjust the following line to match your project structure.
          */
@@ -51,7 +52,11 @@ module.exports = {
     ],
     darkMode: false, // or 'media' or 'class'
     theme: {
-        extend: {},
+        extend: {
+            backgroundImage: theme => ({
+                'warehouse-bg': "url('/static/warehouse-background.jpg')",
+            })
+        },
     },
     variants: {
         extend: {},
