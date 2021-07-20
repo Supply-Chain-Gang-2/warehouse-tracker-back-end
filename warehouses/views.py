ae67790f284a71cac7fb93137876e21c2c1be18f
SkylerJohnson102020 <basseuph@gmail.com>
2021-07-20 15:16:05 -0700

    DON NOT MERGE TO DEV

--- warehouses/views.py
+++ warehouses/views.py
@@ -7,14 +7,14 @@ from .permissions import IsOwnerOrReadOnly
 from .serializers import WarehouseSerializer
 
 class WarehouseList(ListCreateAPIView):
-    def get_queryset(self):
-        user = self.request.user
-        queryset = Warehouse.objects.filter(owner=user)
-        return queryset
+    # def get_queryset(self):
+    #     user = self.request.user
+    #     queryset = Warehouse.objects.filter(owner=user)
+    #     return queryset
     serializer_class = WarehouseSerializer
     template_name = "my_warehouses.html"
     model = Warehouse
-    
+
 class WarehouseDetail(RetrieveUpdateDestroyAPIView):
     permission_classes = (IsOwnerOrReadOnly,)
     def get_queryset(self):
