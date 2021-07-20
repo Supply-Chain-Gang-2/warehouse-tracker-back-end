f96cbb5006ae2d5d3fa02575b686501d65378bd1
Kyle Hoac <hoackyle@gmail.com>
2021-07-20 13:21:55 -0700

    got front end to show api data

--- warehouses/views.py
+++ warehouses/views.py
@@ -8,15 +8,22 @@ from .serializers import WarehouseSerializer
 
 
 class WarehouseList(ListCreateAPIView):
-    queryset = Warehouse.objects.all()
+    def get_queryset(self):
+        user = self.request.user
+        queryset = Warehouse.objects.filter(owner=user)
+        return queryset
     serializer_class = WarehouseSerializer
     template_name = "my_warehouses.html"
     model = Warehouse
 
 class WarehouseDetail(RetrieveUpdateDestroyAPIView):
     permission_classes = (IsOwnerOrReadOnly,)
-    queryset = Warehouse.objects.all()
+    def get_queryset(self):
+        user = self.request.user
+        queryset = Warehouse.objects.filter(owner=user)
+        return queryset
     serializer_class = WarehouseSerializer
     template_name = "my_warehouses.html"
     model = Warehouse
+
 
