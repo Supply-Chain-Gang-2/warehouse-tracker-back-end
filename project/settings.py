0d176949aa8c32ffabe52125e71c5d02b3ec568b
Kyle Hoac <hoackyle@gmail.com>
2021-07-22 16:33:29 -0700

    removed dist path from config files

--- project/settings.py
+++ project/settings.py
@@ -151,7 +151,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = BASE_DIR / "morestaticfiles"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [
     BASE_DIR / "theme" / "static",
 ]
