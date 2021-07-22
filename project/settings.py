04f8e2ec5b2fb5aeecb4362f8463139baf33b159
Kyle Hoac <hoackyle@gmail.com>
2021-07-22 16:14:10 -0700

    static files test

--- project/settings.py
+++ project/settings.py
@@ -151,7 +151,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = BASE_DIR / "morestaticfiles"
 STATICFILES_DIRS = [
     BASE_DIR / "theme" / "static",
 ]
