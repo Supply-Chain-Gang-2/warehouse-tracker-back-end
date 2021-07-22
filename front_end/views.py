235065d245c506af481da5e8f52008b84f821f38
Zach Hornung <Zachhornung@comcast.net>
2021-07-22 14:24:18 -0700

    items are rendering

--- front_end/views.py
+++ front_end/views.py
@@ -16,8 +16,7 @@ class WarehouseDetail(DetailView):
     model = Warehouse
     def get_context_data(self, **kwargs):
         context = super(WarehouseDetail, self).get_context_data(**kwargs)
-        context['warehouse'] = Warehouse.objects.all()
-        context['item'] = Item.objects.all()
+        context['items'] = Item.objects.all()
         return context
 
 class WarehouseCreate(CreateView):
